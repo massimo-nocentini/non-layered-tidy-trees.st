9d709748c792d21a110b769391aeab3c6dfa2a09
Massimo Nocentini <massimo.nocentini@gmail.com>
2022-11-23 16:01:39 +0100

    Manually add lua test data

---
+++ lua-test-data/small-tree.lua
@@ -0,0 +1,17 @@
+spec = {
+["doC6dnl"] = { w = 84.09333333333333, h = 21, c = {}, idx = 1, },
+["doC6eyv"] = { w = 328.0, h = 32.9, c = {}, idx = 2, },
+["doCh0k5"] = { w = 185.94666666666663, h = 21, c = {}, idx = 3, },
+["foCcmsslw7m"] = { w = 88.696, h = 25, c = {"doC6eyv","doCh0k5",}, idx = 4, },
+["foCcmsslw7o"] = { w = 107.41600000000001, h = 25, c = {}, idx = 5, },
+["foCcmsslw7p"] = { w = 97.70400000000001, h = 25, c = {}, idx = 6, },
+["doC87sh"] = { w = 259.32, h = 21, c = {}, idx = 7, },
+["doC87si"] = { w = 285.84000000000003, h = 21, c = {}, idx = 8, },
+["doC87u6"] = { w = 87.88000000000001, h = 21, c = {}, idx = 9, },
+["doC87ug"] = { w = 225.81333333333333, h = 21, c = {}, idx = 10, },
+["doC87uv"] = { w = 186.0533333333333, h = 21, c = {}, idx = 11, },
+["doC87vr"] = { w = 178.34666666666666, h = 21, c = {}, idx = 12, },
+["foCcmsslw7q"] = { w = 161.56, h = 25, c = {}, idx = 13, },
+["foCcmsslw7n"] = { w = 255.19200000000004, h = 25, c = {"doC87sh","doC87si","doC87u6","doC87ug","doC87uv","doC87vr","foCcmsslw7q",}, idx = 14, },
+["foCcmssi4sv"] = { w = 337.8666666666666, h = 46, c = {"doC6dnl","foCcmsslw7m","foCcmsslw7o","foCcmsslw7p","foCcmsslw7n",}, idx = 15, },
+}
